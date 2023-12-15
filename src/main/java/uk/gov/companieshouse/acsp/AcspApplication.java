3e392a7a9c5d22dce7d1650f128d8a3f8075bdcb
swalke-ch <swalke@companieshouse.gov.uk>
2023-12-15 10:22:14 +0000

    Add transaction service client

---
+++ src/main/java/uk/gov/companieshouse/acsp/AcspApplication.java
@@ -0,0 +1,23 @@
+package uk.gov.companieshouse.acsp;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class AcspApplication {
+
+	public static final String APP_NAMESPACE = "acsp-api";
+	public static void main(String[] args) {
+		SpringApplication.run(AcspApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		// This is to prevent times being out of time by an hour during British Summer Time in MongoDB
+		// MongoDB stores UTC datetime, and LocalDate doesn't contain timezone
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+}
