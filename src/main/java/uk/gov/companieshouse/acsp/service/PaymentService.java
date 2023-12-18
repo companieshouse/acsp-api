4a923295fd2beaeb9a5719a7e8b7dc6b7fc6b841
abhiahirv1 <aahir@companieshouse.gov.uk>
2023-12-18 14:25:22 +0000

    Add an API client to Payment API

--- src/main/java/uk/gov/companieshouse/acsp/service/PaymentService.java
+++ src/main/java/uk/gov/companieshouse/acsp/service/PaymentService.java
@@ -2,32 +2,46 @@ package uk.gov.companieshouse.acsp.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import uk.gov.companieshouse.acsp.PaymentsClient;
+import uk.gov.companieshouse.acsp.Exception.ServiceException;
 import uk.gov.companieshouse.acsp.model.PaymentDataReponse;
 import uk.gov.companieshouse.acsp.model.PaymentDataRequest;
+import uk.gov.companieshouse.acsp.sdk.ApiClientService;
+import uk.gov.companieshouse.api.error.ApiErrorResponseException;
+import uk.gov.companieshouse.api.handler.exception.URIValidationException;
 
 @Service
 public class PaymentService {
 
     private static final Logger LOG = LoggerFactory.getLogger(PaymentService.class);
 
-    private final PaymentsClient paymentsClient;
+    private static final String EXCEPTION_MESSAGE = "Error Retrieving Payment for transactionId %s";
+    private static final String EXCEPTION_MESSAGE_WITH_HTTP_CODE = EXCEPTION_MESSAGE + ", http status code %s";
 
-    public PaymentService(PaymentsClient paymentsClient) {
-        this.paymentsClient = paymentsClient;
+    private final ApiClientService apiClientService;
+
+    @Autowired
+    public PaymentService(ApiClientService apiClientService) {
+        this.apiClientService = apiClientService;
     }
 
-    public String paymentStatus(PaymentDataRequest paymentDataRequest, String paymentReference) {
-        PaymentDataReponse paymentDataReponse = paymentsClient.createPayment(paymentDataRequest, paymentReference);
-
-        if (paymentDataReponse != null) {
-            return paymentDataReponse.getStatus();
+    public String paymentStatus(PaymentDataRequest paymentDataRequest, String paymentReference) throws ServiceException {
+        try {
+            String uri = "/private/payments/" + paymentReference + "/payment-details";
+            return apiClientService.postApiClient(uri, paymentDataRequest).payment().get(uri).execute().getData().getStatus();
+        } catch (URIValidationException e) {
+            throw new ServiceException(String.format(EXCEPTION_MESSAGE, paymentReference), e);
+        } catch (ApiErrorResponseException e) {
+            if (HttpStatus.NOT_FOUND.value() == e.getStatusCode()) {
+                throw new ServiceException("Payment request failed due to " + e.getMessage());
+            }
+            var message = String.format(
+                    EXCEPTION_MESSAGE_WITH_HTTP_CODE,
+                    paymentReference,
+                    e.getStatusCode());
+            throw new ServiceException(message, e);
         }
-        return null;
-    }
-
-    public PaymentDataReponse getPayment(PaymentDataRequest paymentDataRequest) {
-        return null;
     }
 }
