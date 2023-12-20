678451c3b0e693250b80a82cd3b0792f88dfd57c
swalke-ch <swalke@companieshouse.gov.uk>
2023-12-20 16:14:31 +0000

    Add get transaction api

--- src/main/java/uk/gov/companieshouse/acsp/service/TransactionService.java
+++ src/main/java/uk/gov/companieshouse/acsp/service/TransactionService.java
@@ -1,11 +1,14 @@
 package uk.gov.companieshouse.acsp.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.acsp.Exception.ServiceException;
 import uk.gov.companieshouse.acsp.sdk.ApiClientService;
+import uk.gov.companieshouse.api.ApiClient;
 import uk.gov.companieshouse.api.handler.exception.URIValidationException;
 import uk.gov.companieshouse.api.model.transaction.Transaction;
+import uk.gov.companieshouse.sdk.manager.ApiSdkManager;
 
 import java.io.IOException;
 
@@ -26,6 +29,13 @@ public class TransactionService {
         } catch (URIValidationException | IOException e) {
             throw new ServiceException("Error Retrieving Transaction " + transactionId, e);
         }
+    }
+
+    public Transaction getTransaction(HttpServletRequest request, String transactionsUri) throws IOException, URIValidationException {
+
+        String passthroughHeader = request.getHeader(ApiSdkManager.getEricPassthroughTokenHeader());
+        ApiClient apiClient = apiClientService.getApiClient(passthroughHeader);
+        return apiClient.transactions().get(transactionsUri).execute().getData();
     }
 
     public void updateTransaction(Transaction transaction, String passthroughHeader) throws ServiceException {
