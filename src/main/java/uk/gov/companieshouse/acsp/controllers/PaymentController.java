ccbf77a0320bee76e5854ef6ac801497e59d4258
abhiahirv1 <aahir@companieshouse.gov.uk>
2023-12-21 12:03:42 +0000

    Added changes to controller

--- src/main/java/uk/gov/companieshouse/acsp/controllers/PaymentController.java
+++ src/main/java/uk/gov/companieshouse/acsp/controllers/PaymentController.java
@@ -1,25 +1,31 @@
 package uk.gov.companieshouse.acsp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriTemplate;
 import uk.gov.companieshouse.acsp.Exception.ServiceException;
 import uk.gov.companieshouse.acsp.model.PaymentDataRequest;
 import uk.gov.companieshouse.acsp.service.PaymentService;
+import uk.gov.companieshouse.api.error.ApiErrorResponseException;
+import uk.gov.companieshouse.api.handler.exception.URIValidationException;
 import uk.gov.companieshouse.api.model.payment.PaymentApi;
+import uk.gov.companieshouse.api.model.payment.PaymentSessionApi;
 
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping(consumes = "application/json", produces = "application/json")
 public class PaymentController {
 
+    private static final UriTemplate GET_PAYMENT_URI = new UriTemplate("/payments");
     @Autowired
     private PaymentService paymentService;
 
-    @PostMapping(path="/payments")
-    public String createPayment(@Valid @RequestBody PaymentDataRequest paymentDataRequest) throws ServiceException {
-        String paymentStatus = paymentService.createPaymentStatus(paymentDataRequest);
-        return paymentStatus;
+    @PostMapping(value = "/payments")
+    public ResponseEntity createPayment(@Valid @RequestBody PaymentSessionApi paymentSessionApi) throws ApiErrorResponseException, URIValidationException {
+        String paymentUri = GET_PAYMENT_URI.toString();
+        PaymentApi paymentApi = paymentService.createPaymentStatus(paymentUri, paymentSessionApi);
+        return ResponseEntity.ok(paymentApi);
     }
 
     @GetMapping(path = "/{transaction-id}/payments")
