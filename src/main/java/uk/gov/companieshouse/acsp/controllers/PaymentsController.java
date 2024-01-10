35647dc7fd44b310799ab757e536ac6a9ac87ea8
sardhani <sardhani@companieshouse.gov.uk>
2024-01-10 09:47:59 +0000

    [IDVA5-370]- integration with payments

--- src/main/java/uk/gov/companieshouse/acsp/controllers/PaymentsController.java
+++ src/main/java/uk/gov/companieshouse/acsp/controllers/PaymentsController.java
@@ -4,14 +4,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.companieshouse.acsp.Exception.ServiceException;
 import uk.gov.companieshouse.acsp.service.PaymentsService;
 import uk.gov.companieshouse.acsp.service.TransactionService;
-import uk.gov.companieshouse.api.handler.exception.URIValidationException;
 import uk.gov.companieshouse.api.model.payment.PaymentApi;
 import uk.gov.companieshouse.api.model.transaction.Transaction;
 import uk.gov.companieshouse.sdk.manager.ApiSdkManager;
@@ -25,15 +23,14 @@ public class PaymentsController {
     @Autowired
     private PaymentsService paymentsService;
 
-    @GetMapping(value = "/transaction/payments/{id}")
-    public ResponseEntity<Object> closeTransaction(@PathVariable String id, HttpServletRequest request) throws IOException, URIValidationException {
+    @PostMapping(value = "/transaction/payments/{id}")
+    public ResponseEntity<Object> getPaymentDetails(@PathVariable String id, HttpServletRequest request) {
         try {
             String passThroughHeader = request.getHeader(ApiSdkManager.getEricPassthroughTokenHeader());
             Transaction transaction = transactionService.getTransaction(passThroughHeader, id);
 
-            PaymentApi paymentsDetails = paymentsService.getPaymentsDetails(passThroughHeader, transaction);
+            PaymentApi paymentsDetails = paymentsService.createPaymentSession(passThroughHeader, transaction);
 
-            boolean isPaymentRequired =  transactionService.closeTransaction(transaction);
             return ResponseEntity.ok().body(paymentsDetails);
         } catch (ServiceException e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
---
+++ src/test/java/uk/gov/companieshouse/acsp/service/PaymentsServiceTest.java
@@ -0,0 +1,103 @@
+package uk.gov.companieshouse.acsp.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.companieshouse.acsp.Exception.ServiceException;
+import uk.gov.companieshouse.acsp.sdk.ApiClientService;
+import uk.gov.companieshouse.api.ApiClient;
+import uk.gov.companieshouse.api.error.ApiErrorResponseException;
+import uk.gov.companieshouse.api.handler.exception.URIValidationException;
+import uk.gov.companieshouse.api.handler.payment.PaymentResourceHandler;
+import uk.gov.companieshouse.api.handler.payment.request.PaymentCreate;
+import uk.gov.companieshouse.api.model.ApiResponse;
+import uk.gov.companieshouse.api.model.payment.PaymentApi;
+import uk.gov.companieshouse.api.model.payment.PaymentSessionApi;
+import uk.gov.companieshouse.api.model.transaction.Transaction;
+import uk.gov.companieshouse.api.model.transaction.TransactionLinks;
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PaymentsServiceTest {
+    @Mock
+    private ApiClientService apiClientService;
+
+    @Mock
+    private ApiClient apiClient;
+
+    @Mock
+    private PaymentResourceHandler paymentResourceHandler;
+
+    @Mock
+    private PaymentCreate paymentCreate;
+
+    @Mock
+    private ApiResponse<PaymentApi> response;
+
+    @InjectMocks
+    private PaymentsService paymentsService;
+
+    private static final String TRANSACTION_ID = "12345678";
+
+    private Transaction transaction;
+
+    @BeforeEach
+    void setUp(){
+        transaction = new Transaction();
+        transaction.setId(TRANSACTION_ID);
+        TransactionLinks transactionLinks = new TransactionLinks();
+        transactionLinks.setPayment("/payments");
+        transaction.setLinks(transactionLinks);
+    }
+    @Test
+    void getPaymentDetails_Success() throws IOException, URIValidationException, ServiceException {
+        PaymentApi paymentApi = new PaymentApi();
+        paymentApi.setAmount("100");
+
+        when(apiClientService.getApiClient(null)).thenReturn(apiClient);
+        when(apiClient.payment()).thenReturn(paymentResourceHandler);
+        when(paymentResourceHandler.create(anyString(), any(PaymentSessionApi.class))).thenReturn(paymentCreate);
+        when(paymentCreate.execute()).thenReturn(response);
+        when(response.getData()).thenReturn(paymentApi);
+
+        assertEquals(paymentApi,paymentsService.createPaymentSession(null, transaction));
+    }
+
+    @Test
+    void getPaymentDetails_IOException() throws IOException {
+
+        when(apiClientService.getApiClient(null)).thenThrow(new IOException("ERROR"));
+        assertThrows(RuntimeException.class,() -> paymentsService.createPaymentSession(null, transaction));
+    }
+
+    @Test
+    void getPaymentDetails_URIValidationException() throws IOException, URIValidationException {
+
+        when(apiClientService.getApiClient(null)).thenReturn(apiClient);
+        when(apiClient.payment()).thenReturn(paymentResourceHandler);
+        when(paymentResourceHandler.create(anyString(), any(PaymentSessionApi.class))).thenReturn(paymentCreate);
+        when(paymentCreate.execute()).thenThrow(new URIValidationException("ERROR"));
+
+        assertThrows(RuntimeException.class, () -> {
+            paymentsService.createPaymentSession(null, transaction);
+        });
+    }
+
+    @Test
+    void getPaymentDetails_ApiErrorResponseException() throws IOException, URIValidationException {
+
+        when(apiClientService.getApiClient(null)).thenReturn(apiClient);
+        when(apiClient.payment()).thenReturn(paymentResourceHandler);
+        when(paymentResourceHandler.create(anyString(), any(PaymentSessionApi.class))).thenReturn(paymentCreate);
+        when(paymentCreate.execute()).thenThrow(ApiErrorResponseException.fromIOException(new IOException("ERROR")));
+
+        assertThrows(RuntimeException.class, () -> paymentsService.createPaymentSession(null, transaction));
+    }
+}
