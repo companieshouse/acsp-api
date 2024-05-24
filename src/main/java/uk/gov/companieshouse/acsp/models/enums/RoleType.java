b3d1930f8f4e4abfd535a9ff18da0eaa825ea397
ttingle-ch <ttingle@companieshouse.gov.uk>
2024-05-24 15:35:11 +0100

    Updated RoleType enum to include all role types

--- src/main/java/uk/gov/companieshouse/acsp/models/enums/RoleType.java
+++ src/main/java/uk/gov/companieshouse/acsp/models/enums/RoleType.java
@@ -2,10 +2,13 @@ package uk.gov.companieshouse.acsp.models.enums;
 
 public enum RoleType {
     SOLE_TRADER("Sole Trader"),
+    MEMBER_OF_PARTNERSHIP("Member of partnership"),
+    MEMBER_OF_GOVERNING_BODY("Member of the governing body"),
+    EQUIVALENT_OF_DIRECTOR("Equivalent to director"),
+    MEMBER_OF_ENTITY("Member of the entity"),
     DIRECTOR("Director"),
+    MEMBER_OF_LLP("Member of LLP"),
     GENERAL_PARTNER("General partner"),
-    MEMBER("Member"),
-    GOVERNING_BODY_MEMBER("Member of the governing body"),
     SOMEONE_ELSE("Someone else");
 
     public final String label;
