c25c058d0b8b54dbff6c4afad745d8ae83774aa8
sampankumar <skumar@companieshouse.gov.uk>
2024-05-29 12:00:42 +0100

    Removing code smell

--- src/main/java/uk/gov/companieshouse/acsp/service/FilingsService.java
+++ src/main/java/uk/gov/companieshouse/acsp/service/FilingsService.java
@@ -74,19 +74,17 @@ public class FilingsService {
 
   private void setFilingApiData(FilingApi filing, String acspApplicationId, String transactionId,
                                 String passThroughTokenHeader) throws ServiceException {
-    if(acspService.getAcsp(acspApplicationId).isPresent()) {
-      var acspDataDto = acspService.getAcsp(acspApplicationId).orElse(null);
-      if(acspDataDto != null) {
-        var data = new HashMap<String, Object>();
-        var transaction = transactionService.getTransaction(passThroughTokenHeader, transactionId);
-        buildPresenter(data, acspDataDto);
-        buildSubmission(data, acspDataDto, transactionId);
-        buildAcspData(data, acspDataDto);
-        buildItem(data, transactionId);
-        setPaymentData(data, transaction, passThroughTokenHeader);
-        setDescriptionFields(filing);
-        buildFilingStatus(filing, data);
-      }
+    var acspDataDto = acspService.getAcsp(acspApplicationId).orElse(null);
+    if(acspDataDto != null) {
+      var data = new HashMap<String, Object>();
+      var transaction = transactionService.getTransaction(passThroughTokenHeader, transactionId);
+      buildPresenter(data, acspDataDto);
+      buildSubmission(data, acspDataDto, transactionId);
+      buildAcspData(data, acspDataDto);
+      buildItem(data, transactionId);
+      setPaymentData(data, transaction, passThroughTokenHeader);
+      setDescriptionFields(filing);
+      buildFilingStatus(filing, data);
     }
   }
 
