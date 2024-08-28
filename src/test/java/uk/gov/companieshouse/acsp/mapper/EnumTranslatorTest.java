35de5e7e6d8b6d54651ee3d7b662d73aee4ae6a4
amanueltedros <atedros@companieshouse.gov.uk>
2024-08-28 14:44:35 +0100

    more unit tests added

--- src/test/java/uk/gov/companieshouse/acsp/mapper/EnumTranslatorTest.java
+++ src/test/java/uk/gov/companieshouse/acsp/mapper/EnumTranslatorTest.java
@@ -10,60 +10,83 @@ import uk.gov.companieshouse.acsp.models.enums.TypeOfBusiness;
 
 @SpringBootTest
 public class EnumTranslatorTest {
-	@Autowired
-	private EnumTranslator enumTranslator;
+    @Autowired
+    private EnumTranslator enumTranslator;
 
     @Test
-	void typeOfBusinessEnumToString() {
-		TypeOfBusiness inputEnum = TypeOfBusiness.PARTNERSHIP;
-		String expected = "non-registered-partnership";
-		String actual = enumTranslator.typeOfBusinessEnumToString(inputEnum);
+    void typeOfBusinessEnumToString() {
+        TypeOfBusiness inputEnum = TypeOfBusiness.PARTNERSHIP;
+        String expected = "non-registered-partnership";
+        String actual = enumTranslator.typeOfBusinessEnumToString(inputEnum);
 
-		assertEquals(expected, actual);
-	}
+        assertEquals(expected, actual);
+    }
+    @Test
+    void typeOfBusinessEnumToStringNull() {
+        TypeOfBusiness inputEnum = TypeOfBusiness.PARTNERSHIP;
+        String expected = "non-registered-partnership";
+        String actual = enumTranslator.typeOfBusinessEnumToString(inputEnum);
 
-	@Test
-	 void typeOfBusinessStringToEnum() {
-		String inputEnum = TypeOfBusiness.LC.label;
-		TypeOfBusiness expected = TypeOfBusiness.LC;
-		TypeOfBusiness actual = enumTranslator.typeOfBusinessStringToEnum(inputEnum);
+        assertEquals(expected, actual);
+    }
 
-		assertEquals(expected, actual);
-	}
+    @Test
+    void typeOfBusinessStringToEnum() {
+        String inputEnum = TypeOfBusiness.LC.label;
+        TypeOfBusiness expected = TypeOfBusiness.LC;
+        TypeOfBusiness actual = enumTranslator.typeOfBusinessStringToEnum(inputEnum);
 
-	@Test
-	 void workSectorEnumToString() {
-		BusinessSector inputEnum = BusinessSector.CI;
-		String expected = "credit-institutions";
-		String actual = enumTranslator.businessSectorEnumToString(inputEnum);
+        assertEquals(expected, actual);
+    }
 
-		assertEquals(expected, actual);
-	}
-	@Test
-	 void workSectorStringToEnum() {
-		String inputEnum = BusinessSector.AIA.label;
-		BusinessSector expected = BusinessSector.AIA;
-		BusinessSector actual = enumTranslator.businessSectorStringToEnum(inputEnum);
+    @Test
+    void workSectorEnumToString() {
+        BusinessSector inputEnum = BusinessSector.CI;
+        String expected = "credit-institutions";
+        String actual = enumTranslator.businessSectorEnumToString(inputEnum);
 
-		assertEquals(expected, actual);
-	}
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void workSectorEnumToNullString() {
+        String expected = "";
+        String actual = enumTranslator.businessSectorEnumToString(null);
 
-	@Test
-	 void roleTypeEnumToString() {
-		RoleType inputEnum = RoleType.GENERAL_PARTNER;
-		String expected = "general-partner";
-		String actual = enumTranslator.roleTypeEnumToString(inputEnum);
+        assertEquals(expected, actual);
+    }
+    @Test
+    void workSectorStringToEnum() {
+        String inputEnum = BusinessSector.AIA.label;
+        BusinessSector expected = BusinessSector.AIA;
+        BusinessSector actual = enumTranslator.businessSectorStringToEnum(inputEnum);
 
-		assertEquals(expected, actual);
-	}
-	@Test
-	 void roleTypeStringToEnum() {
-		String inputEnum = RoleType.DIRECTOR.label;
-		RoleType expected = RoleType.DIRECTOR;
-		RoleType actual = enumTranslator.roleTypeStringToEnum(inputEnum);
+        assertEquals(expected, actual);
+    }
+    @Test
+    void roleTypeEnumToString() {
+        RoleType inputEnum = RoleType.GENERAL_PARTNER;
+        String expected = "general-partner";
+        String actual = enumTranslator.roleTypeEnumToString(inputEnum);
 
-		assertEquals(expected, actual);
-	}
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void roleTypeEnumToNullString() {
+        String expected = "";
+        String actual = enumTranslator.roleTypeEnumToString(null);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void roleTypeStringToEnum() {
+        String inputEnum = RoleType.DIRECTOR.label;
+        RoleType expected = RoleType.DIRECTOR;
+        RoleType actual = enumTranslator.roleTypeStringToEnum(inputEnum);
+
+        assertEquals(expected, actual);
+    }
 
 }
+
