c7a371a84220fdcda60a27407bf44f2904f085d0
ttingle-ch <ttingle@companieshouse.gov.uk>
2024-10-11 14:58:19 +0100

    Removed code smell

--- src/test/java/uk/gov/companieshouse/acsp/interceptor/AuthenticationInterceptorTest.java
+++ src/test/java/uk/gov/companieshouse/acsp/interceptor/AuthenticationInterceptorTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class AuthenticationInterceptorTest {
+class AuthenticationInterceptorTest {
 
     private AuthenticationInterceptor authenticationInterceptor;
 
