bd83eb4ac96b4ebc19f1989df3f79858b3428712
rdas <rdas@companieshouse.gov.uk>
2024-10-18 14:18:53 +0100

    IDVA5-1251 [Registration- analysis] Cost Endpoint

--- src/main/java/uk/gov/companieshouse/acsp/service/CostService.java
+++ src/main/java/uk/gov/companieshouse/acsp/service/CostService.java
@@ -4,20 +4,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.api.model.payment.Cost;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 @Service
 public class CostService {
 
     @Value("${ACSP01_COST}")
     private String costAmount;
+    @Value("${ACSP01_TYPES}")
+    private List<String> paymentTypes;
     private static final String COST_DESC = "Acsp fee";
     private static final String PAYMENT_ACCOUNT = "data-maintenance";
 
     public Cost getCosts() {
         var cost = new Cost();
         cost.setAmount(costAmount);
-        cost.setAvailablePaymentMethods(Collections.singletonList("credit-card"));
+        cost.setAvailablePaymentMethods(paymentTypes);
         cost.setClassOfPayment(Collections.singletonList(PAYMENT_ACCOUNT));
         cost.setDescription(COST_DESC);
         cost.setDescriptionIdentifier("description-identifier");
