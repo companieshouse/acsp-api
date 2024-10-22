5dcc2883fed01afa35a24b297a0ffbfa2231abaa
amanueltedros <atedros@companieshouse.gov.uk>
2024-10-22 12:46:41 +0100

    new outer modal added for acspDto

--- src/main/java/uk/gov/companieshouse/acsp/mapper/ACSPRegDataDtoDaoMapper.java
+++ src/main/java/uk/gov/companieshouse/acsp/mapper/ACSPRegDataDtoDaoMapper.java
@@ -4,7 +4,9 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.acsp.models.dao.AcspDataDao;
+import uk.gov.companieshouse.acsp.models.dao.DataDao;
 import uk.gov.companieshouse.acsp.models.dto.AcspDataDto;
+import uk.gov.companieshouse.acsp.models.dto.DataMapper;
 
 @Component
 @Mapper(componentModel = "spring", uses = EnumTranslator.class)
@@ -12,10 +14,10 @@ public interface ACSPRegDataDtoDaoMapper {
       @Mapping(source = "typeOfBusiness", target = "typeOfBusiness", qualifiedByName = "TypeOfBusinessStringToEnum")
       @Mapping(source = "roleType", target = "roleType", qualifiedByName = "RoleTypeStringToEnum")
       @Mapping(source = "workSector", target = "workSector", qualifiedByName = "WorkSectorStringToEnum")
-      AcspDataDto daoToDto(AcspDataDao acspDataDao);
+      AcspDataDto daoToDto(DataDao dataDao);
 
-      @Mapping(source = "typeOfBusiness", target = "typeOfBusiness", qualifiedByName = "TypeOfBusinessEnumToString")
-      @Mapping(source = "roleType", target = "roleType", qualifiedByName = "RoleTypeEnumToString")
-      @Mapping(source = "workSector", target = "workSector", qualifiedByName = "WorkSectorEnumToString")
-      AcspDataDao dtoToDao(AcspDataDto acspDataDto);
+      @Mapping(source = "data.typeOfBusiness", target = "data.typeOfBusiness", qualifiedByName = "TypeOfBusinessEnumToString")
+      @Mapping(source = "data.roleType", target = "data.roleType", qualifiedByName = "RoleTypeEnumToString")
+      @Mapping(source = "data.workSector", target = "data.workSector", qualifiedByName = "WorkSectorEnumToString")
+      AcspDataDao dtoToDao(DataMapper dataMapper);
 }
---
+++ src/main/java/uk/gov/companieshouse/acsp/models/dto/DataMapper.java
@@ -0,0 +1,29 @@
+package uk.gov.companieshouse.acsp.models.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import org.springframework.data.annotation.Id;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class DataMapper {
+   @Id
+    private String id;
+
+    private AcspDataDto data;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public AcspDataDto getData() {
+        return data;
+    }
+
+    public void setData(AcspDataDto data) {
+        this.data = data;
+    }
+}
