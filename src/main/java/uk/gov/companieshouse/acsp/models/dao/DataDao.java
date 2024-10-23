f370ecd7928a6143ce21b51e9cbd0bc1b32c1af1
amanueltedros <atedros@companieshouse.gov.uk>
2024-10-23 09:30:40 +0100

    unused import removed

--- src/main/java/uk/gov/companieshouse/acsp/models/dao/DataDao.java
+++ src/main/java/uk/gov/companieshouse/acsp/models/dao/DataDao.java
@@ -1,7 +1,6 @@
 package uk.gov.companieshouse.acsp.models.dao;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
