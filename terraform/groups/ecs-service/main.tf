36dfa1392d27b8321395a8679ebd31c39b94af80
kullas-ch <kullas@companieshouse.gov.uk>
2025-01-06 16:39:21 +0000

    Updating autoscaling to 70 and healthcheck grace period to 420

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -72,7 +72,7 @@ module "ecs-service" {
   use_task_container_healthcheck    = true
   healthcheck_path                  = local.healthcheck_path
   healthcheck_matcher               = local.healthcheck_matcher
-  health_check_grace_period_seconds = 300
+  health_check_grace_period_seconds = 420
   healthcheck_healthy_threshold     = "2"
 
   # Service performance and scaling configs
