3bff1ef32f4ff2fd899c00c2a6d4e7b8882df806
swalke-ch <swalke@companieshouse.gov.uk>
2025-04-08 14:14:02 +0100

    downgrade the kafka version

--- src/main/java/uk/gov/companieshouse/acsp/controllers/VerifyClientController.java
+++ src/main/java/uk/gov/companieshouse/acsp/controllers/VerifyClientController.java
@@ -7,11 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.companieshouse.acsp.models.email.ClientVerificationEmailData;
 import uk.gov.companieshouse.acsp.service.EmailService;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
+
+import static uk.gov.companieshouse.acsp.AcspApplication.APP_NAMESPACE;
 
 @RestController
 @RequestMapping("/acsp-api/verify-client-identity")
 public class VerifyClientController {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(APP_NAMESPACE);
     private final EmailService emailService;
 
     public VerifyClientController(EmailService emailService) {
@@ -28,6 +32,7 @@ public class VerifyClientController {
                     clientVerificationEmailDao.getClientEmailAddress());
             return ResponseEntity.ok().build();
         } catch (Exception e) {
+            LOGGER.error("Error sending email for identity-verification " + e.getMessage());
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
