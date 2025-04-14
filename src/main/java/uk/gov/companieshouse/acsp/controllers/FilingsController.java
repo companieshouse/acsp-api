1b6c912ebe12cb7f602535de5a5cdf3d5d0672ce
ttingle-ch <ttingle@companieshouse.gov.uk>
2025-04-14 15:26:31 +0100

    Reverting changes to filings endpoint

--- src/main/java/uk/gov/companieshouse/acsp/controllers/FilingsController.java
+++ src/main/java/uk/gov/companieshouse/acsp/controllers/FilingsController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import uk.gov.companieshouse.acsp.exception.ServiceException;
 import uk.gov.companieshouse.acsp.exception.SubmissionNotLinkedToTransactionException;
-import uk.gov.companieshouse.acsp.models.filing.Filing;
 import uk.gov.companieshouse.acsp.service.FilingsService;
+import uk.gov.companieshouse.api.model.filinggenerator.FilingApi;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 import uk.gov.companieshouse.sdk.manager.ApiSdkManager;
@@ -32,7 +32,7 @@ public class FilingsController {
     }
 
     @GetMapping
-    public ResponseEntity<Filing> getFiling(
+    public ResponseEntity<FilingApi[]> getFiling(
             @PathVariable(ACSP_APPLICATION_ID_KEY) String acspApplicationId,
             @PathVariable(TRANSACTION_ID_KEY) String transactionId,
             HttpServletRequest request) {
@@ -40,8 +40,8 @@ public class FilingsController {
 
         String passThroughTokenHeader = request.getHeader(ApiSdkManager.getEricPassthroughTokenHeader());
         try {
-            Filing filing = filingService.generateAcspApplicationFiling(acspApplicationId, transactionId, passThroughTokenHeader);
-            return ResponseEntity.ok(filing);
+            FilingApi filing = filingService.generateAcspApplicationFiling(acspApplicationId, transactionId, passThroughTokenHeader);
+            return ResponseEntity.ok(new FilingApi[]{filing});
         } catch (ServiceException | SubmissionNotLinkedToTransactionException e) {
             return ResponseEntity.badRequest().build();
         } catch (Exception e) {
