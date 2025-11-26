85247d4321bc0dba53b7e65608c45133408b54e3
AdeHarrison-ch <aharrison2@companieshouse.gov.uk>
2025-11-26 09:26:54 +0000

    ASM-867: Fixed vulnerabilities

--- src/main/java/uk/gov/companieshouse/acsp/util/TransactionUtils.java
+++ src/main/java/uk/gov/companieshouse/acsp/util/TransactionUtils.java
@@ -1,6 +1,6 @@
 package uk.gov.companieshouse.acsp.util;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.acsp.models.dto.AcspDataDto;
